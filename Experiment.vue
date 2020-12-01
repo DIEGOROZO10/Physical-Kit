eeac9d117b2d8e2664753936bf51b373279c4c39
DIEGOROZO10 <75314604+DIEGOROZO10@users.noreply.github.com>
2020-12-01 08:06:51 -0500

    Add files via upload

---
+++ Experiment.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="view_container">
+    <h2>{{ experiment.name }}</h2>
+    <div id="intro">{{ experiment.introduction }}</div>
+    <br>
+    <h3 class="list_title">Procedimiento</h3>
+    <v-row>
+      <v-col>
+        <ul class="list">
+          <li class="list_item" v-for="item in experiment.instructions" :key="item">{{ item }}</li>
+        </ul>
+      </v-col>
+      <v-col>
+      <div class="video_container">
+        <video width="420" autoplay controls>
+          <source :src="'./videos/' + experiment.videoName" type="video/mp4" />
+        </video>
+        <video width="420" autoplay controls>
+          <source :src="'./videos/' + experiment.videoName2" type="video/mp4" />
+        </video>
+      </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+  name: "Experiment",
+  methods: {
+  },
+  computed: {
+    ...mapGetters({
+      experiment: 'getExperiment'
+    })
+  }
+};
+</script>
+<style scoped>
+.view_container {
+  justify-content: flex-start !important;
+  padding: 100px;
+}
+.list {
+  text-align: left;
+}
+#intro{
+  white-space:pre;
+}
+</style>
---
+++ Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="view_container">
+    <div class="subjects-container">
+      <Subject
+        class="subject"
+        title="Mecanica"
+        :experiments="getExperiments('Mecánica')"
+      />
+      <Subject
+        class="subject"
+        title="Magnetismo"
+        :experiments="getExperiments('Magnetismo')"
+      />
+      <Subject
+        class="subject"
+        title="Electronica"
+        :experiments="getExperiments('Electronica')"
+      />
+      <Subject
+        class="subject"
+        title="Ondas"
+        :experiments="getExperiments('Ondas')"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Subject from "../components/Subject.vue";
+import info from "../info";
+
+export default {
+  name: "Home",
+  components: {
+    Subject
+  },
+  methods: {
+    getExperiments(name) {
+      const subject = info.find(item => item.name === name);
+      console.log(info[0].name);
+      return subject ? subject.experiments : [];
+    }
+  }
+};
+</script>
+
+<style>
+.subject {
+  width: 100%;
+  height: 100%;
+}
+.subjects-container {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 50% 50%;
+  gap: 40px;
+  justify-items: center;
+  align-items: center;
+}
+</style>
